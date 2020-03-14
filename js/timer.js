536d14f7ef1879d9c7e349ab24132da946343342
christophbenkler <christoph.benkler@me.com>
2020-03-14 09:58:34 +0100

    update

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 13, 2020 17:00:00").getTime();
+var countDownDate = new Date("Mar 20, 2020 17:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
